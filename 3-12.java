7d2ed4181b3864cfdd73f10f83d69acd652731ac
Hojun Im <ghwnsdla8094@gmail.com>
2023-07-19 13:51:55 +0900

    반복문

---
+++ 3-12.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+
+        while((s=br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a+b);
+        }
+        br.close();
+    }
+}
